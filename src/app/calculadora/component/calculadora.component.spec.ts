01560768fe437e3e5e8e02477c7387dc8705d249
Williams25 <willia.souza.timao@hotmail.com>
2019-12-12 21:31:47 -0300

    serviço calculadora

--- src/app/calculadora/component/calculadora.component.spec.ts
+++ src/app/calculadora/component/calculadora.component.spec.ts
@@ -2,13 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 
+import { CalculadoraService } from '../services'
+
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
   let fixture: ComponentFixture<CalculadoraComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculadoraComponent ]
+      declarations: [ CalculadoraComponent ],
+      providers: [
+        CalculadoraService
+      ]
     })
     .compileComponents();
   }));
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -4,6 +4,41 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
+  // DEFININDO CONSTANTES PARA SOMENTE LEITURA
+  static readonly SOMA: string = '+'
+  static readonly SUBTRACAO: string = '-'
+  static readonly DIVISAO: string = '/'
+  static readonly MULTIPICACAO: string = '*'
 
   constructor() { }
+
+  // METODO CACULAR
+  calcular(num1: number, num2: number, operacao: String): number {
+    let resultado: number
+
+    // VERIFICA O TIPO DA OPERACAO
+    switch (operacao) {
+      // ACESSANDO CONSTANTES 
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2
+        break
+      // ACESSANDO CONSTANTES 
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2
+        break
+      // ACESSANDO CONSTANTES 
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2
+        break
+      // ACESSANDO CONSTANTES 
+      case CalculadoraService.MULTIPICACAO:
+        resultado = num1 * num2
+        break
+      default:
+        resultado = 0
+        break
+    }
+    // RETORNANDO UM VALOR INTEIRO
+    return resultado
+  }
 }
